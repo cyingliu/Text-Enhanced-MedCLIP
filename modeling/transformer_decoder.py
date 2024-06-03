23c353a9303422653a25b6bfa6c652dca5706202
Fan Diao <fan.diao93@gmail.com>
2024-06-03 00:05:58 -0700

    transformer fusion code

---
+++ modeling/transformer_decoder.py
@@ -0,0 +1,183 @@
+import numpy as np
+import copy
+from typing import Any, Optional
+
+import torch
+import torch.nn as nn
+
+from modeling.transformer_layers import *
+from transformers import AutoModel
+from transformers.modeling_outputs import ImageClassifierOutput
+
+"""
+Based on some code (with modification) from CS231N assigment and https://github.com/huggingface/transformers/blob/main/src/transformers/models/clip/modeling_clip.py
+"""
+class VqaTransformerDecoder(nn.Module):
+    def __init__(self, clip_model_name, text_model_path=None, num_labels=2, num_heads=4,
+                 num_layers=2):
+        super().__init__()
+
+        self.base_model = AutoModel.from_pretrained(clip_model_name)
+
+        if text_model_path:
+            print("Load text model weight from:", text_model_path)
+            text_model_dict = torch.load(text_model_path, map_location="cpu")
+            text_model_dict = {k: v for k, v in text_model_dict.items() if k.startswith("text_model")}
+            base_model_dict = self.base_model.state_dict()
+            base_model_dict.update(text_model_dict)
+            self.base_model.load_state_dict(base_model_dict)
+
+        self.num_labels = num_labels
+        projection_dim = self.base_model.projection_dim
+        text_embed_dim = self.base_model.text_embed_dim
+        vision_embed_dim = self.base_model.vision_embed_dim
+        self.eos_token_id = self.base_model.text_model.eos_token_id
+        output_dim = 1 if num_labels == 2 else num_labels # scalar output for binary classification
+
+        decoder_layer = TransformerDecoderLayer(input_dim=projection_dim, num_heads=num_heads)
+        self.transformer = TransformerDecoder(decoder_layer, num_layers=num_layers)
+        self._init_weights(self.transformer)
+
+        self.text_projection = nn.Linear(text_embed_dim, projection_dim)
+        self.vision_projection = nn.Linear(vision_embed_dim, projection_dim)
+        self.output = nn.Linear(projection_dim, output_dim)
+
+    def _init_weights(self, module):
+        """
+        Initialize the weights of the network.
+        """
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+            if isinstance(module, nn.Linear) and module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+
+    def forward(self, 
+                input_ids: Optional[torch.LongTensor] = None,
+                pixel_values: Optional[torch.FloatTensor] = None,
+                attention_mask: Optional[torch.Tensor] = None,
+                position_ids: Optional[torch.LongTensor] = None,
+                labels: Optional[torch.Tensor] = None,):
+        
+        text_features = self.base_model.text_model(input_ids=input_ids,
+                                                          attention_mask=attention_mask,
+                                                          position_ids=position_ids).last_hidden_state # (N, S, 512)
+        text_features = self.text_projection(text_features)
+        image_features = self.base_model.vision_model(pixel_values=pixel_values).last_hidden_state # (N, T, 512)
+        image_features = self.vision_projection(image_features)
+
+        transformer_output = self.transformer(text_features, image_features, None) # shape: (N, T, W)
+        
+        # Source: https://github.com/huggingface/transformers/blob/main/src/transformers/models/clip/modeling_clip.py
+        if self.eos_token_id == 2:
+            pooled_output = transformer_output[
+                torch.arange(transformer_output.shape[0], device=transformer_output.device),
+                input_ids.to(dtype=torch.int, device=transformer_output.device).argmax(dim=-1),
+            ]
+        else:
+            pooled_output = transformer_output[
+                torch.arange(transformer_output.shape[0], device=transformer_output.device),
+                (input_ids.to(dtype=torch.int, device=transformer_output.device) == self.eos_token_id)
+                .int()
+                .argmax(dim=-1),
+            ]
+
+        logits = self.output(pooled_output).squeeze() # shape: (N, T, V)
+        
+        loss = None
+        if labels is not None:
+            labels = labels.to(logits.device)
+            if self.num_labels == 2:
+                # binary classification
+                loss_fct = nn.BCEWithLogitsLoss()
+                loss = loss_fct(logits.squeeze(), labels.squeeze().float())
+            else:
+                # multi-class classification
+                loss_fct = nn.CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            
+        return ImageClassifierOutput(
+            loss=loss,
+            logits=logits,
+        )
+
+class TransformerDecoderLayer(nn.Module):
+    """
+    A single layer of a Transformer decoder, to be used with TransformerDecoder.
+    """
+    def __init__(self, input_dim, num_heads, dim_feedforward=2048, dropout=0.1):
+        """
+        Construct a TransformerDecoderLayer instance.
+
+        Inputs:
+         - input_dim: Number of expected features in the input.
+         - num_heads: Number of attention heads
+         - dim_feedforward: Dimension of the feedforward network model.
+         - dropout: The dropout value.
+        """
+        super().__init__()
+        self.self_attn = MultiHeadAttention(input_dim, num_heads, dropout)
+        self.multihead_attn = MultiHeadAttention(input_dim, num_heads, dropout)
+        self.linear1 = nn.Linear(input_dim, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, input_dim)
+
+        self.norm1 = nn.LayerNorm(input_dim)
+        self.norm2 = nn.LayerNorm(input_dim)
+        self.norm3 = nn.LayerNorm(input_dim)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+
+        self.activation = nn.ReLU()
+
+
+    def forward(self, tgt, memory, tgt_mask=None):
+        """
+        Pass the inputs (and mask) through the decoder layer.
+
+        Inputs:
+        - tgt: the sequence to the decoder layer, of shape (N, T, W)
+        - memory: the sequence from the last layer of the encoder, of shape (N, S, D)
+        - tgt_mask: the parts of the target sequence to mask, of shape (T, T)
+
+        Returns:
+        - out: the Transformer features, of shape (N, T, W)
+        """
+        # Perform self-attention on the target sequence (along with dropout and
+        # layer norm).
+        tgt2 = self.self_attn(query=tgt, key=tgt, value=tgt, attn_mask=tgt_mask)
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm1(tgt)
+
+        # Attend to both the target sequence and the sequence from the last
+        # encoder layer.
+        tgt2 = self.multihead_attn(query=tgt, key=memory, value=memory)
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm2(tgt)
+
+        # Pass
+        tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
+        tgt = tgt + self.dropout3(tgt2)
+        tgt = self.norm3(tgt)
+        return tgt
+
+def clones(module, N):
+    "Produce N identical layers."
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, decoder_layer, num_layers):
+        super().__init__()
+        self.layers = clones(decoder_layer, num_layers)
+        self.num_layers = num_layers
+
+    def forward(self, tgt, memory, tgt_mask=None):
+        output = tgt
+
+        for mod in self.layers:
+            output = mod(output, memory, tgt_mask=tgt_mask)
+
+        return output
---
+++ modeling/transformer_layers.py
@@ -0,0 +1,148 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math
+
+"""
+Code source: CS231N assigment.
+This file defines layer types that are commonly used for transformers.
+"""
+
+class PositionalEncoding(nn.Module):
+    """
+    Encodes information about the positions of the tokens in the sequence. In
+    this case, the layer has no learnable parameters, since it is a simple
+    function of sines and cosines.
+    """
+    def __init__(self, embed_dim, dropout=0.1, max_len=5000):
+        """
+        Construct the PositionalEncoding layer.
+
+        Inputs:
+         - embed_dim: the size of the embed dimension
+         - dropout: the dropout value
+         - max_len: the maximum possible length of the incoming sequence
+        """
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        assert embed_dim % 2 == 0
+        # Create an array with a "batch dimension" of 1 (which will broadcast
+        # across all examples in the batch).
+        pe = torch.zeros(1, max_len, embed_dim)
+
+        for i in range(max_len):
+          for k in range(embed_dim // 2):
+            pe[0, i, k * 2] = math.sin(i * 10000**(-k * 2 / embed_dim))
+            pe[0, i, k * 2 + 1] = math.cos(i * 10000**(-k * 2 / embed_dim))
+
+        # Make sure the positional encodings will be saved with the model
+        # parameters (mostly for completeness).
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Element-wise add positional embeddings to the input sequence.
+
+        Inputs:
+         - x: the sequence fed to the positional encoder model, of shape
+              (N, S, D), where N is the batch size, S is the sequence length and
+              D is embed dim
+        Returns:
+         - output: the input sequence + positional encodings, of shape (N, S, D)
+        """
+        N, S, D = x.shape
+
+        output = torch.empty((N, S, D))
+        output = x + self.pe[0,:S,:]
+        output = self.dropout(output)
+
+        return output
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    A model layer which implements a simplified version of masked attention, as
+    introduced by "Attention Is All You Need" (https://arxiv.org/abs/1706.03762).
+
+    Usage:
+      attn = MultiHeadAttention(embed_dim, num_heads=2)
+
+      # self-attention
+      data = torch.randn(batch_size, sequence_length, embed_dim)
+      self_attn_output = attn(query=data, key=data, value=data)
+
+      # attention using two inputs
+      other_data = torch.randn(batch_size, sequence_length, embed_dim)
+      attn_output = attn(query=data, key=other_data, value=other_data)
+    """
+
+    def __init__(self, embed_dim, num_heads, dropout=0.1):
+        """
+        Construct a new MultiHeadAttention layer.
+
+        Inputs:
+         - embed_dim: Dimension of the token embedding
+         - num_heads: Number of attention heads
+         - dropout: Dropout probability
+        """
+        super().__init__()
+        assert embed_dim % num_heads == 0
+
+        self.key = nn.Linear(embed_dim, embed_dim)
+        self.query = nn.Linear(embed_dim, embed_dim)
+        self.value = nn.Linear(embed_dim, embed_dim)
+        self.proj = nn.Linear(embed_dim, embed_dim)
+        
+        self.attn_drop = nn.Dropout(dropout)
+
+        self.n_head = num_heads
+        self.emd_dim = embed_dim
+        self.head_dim = self.emd_dim // self.n_head
+
+    def forward(self, query, key, value, attn_mask=None):
+        """
+        Calculate the masked attention output for the provided data, computing
+        all attention heads in parallel.
+
+        In the shape definitions below, N is the batch size, S is the source
+        sequence length, T is the target sequence length, and E is the embedding
+        dimension.
+
+        Inputs:
+        - query: Input data to be used as the query, of shape (N, S, E)
+        - key: Input data to be used as the key, of shape (N, T, E)
+        - value: Input data to be used as the value, of shape (N, T, E)
+        - attn_mask: Array of shape (S, T) where mask[i,j] == 0 indicates token
+          i in the source should not influence token j in the target.
+
+        Returns:
+        - output: Tensor of shape (N, S, E) giving the weighted combination of
+          data in value according to the attention weights calculated using key
+          and query.
+        """
+        # print('query shape', query.shape)
+        # print('value shape', value.shape)
+        N, S, E = query.shape
+        N, T, E = value.shape
+        output = torch.empty((N, S, E))
+
+        Q = self.query(query) # shape (N, S, E)
+        K = self.key(key) # shape (N, T, E)
+        V = self.value(value) # shape (N, T, E)
+
+        Q = Q.reshape(N, S, self.n_head, self.head_dim).transpose(1, 2) # shape (N, H, S, E/H)
+        K = K.reshape(N, T, self.n_head, self.head_dim).transpose(1, 2) # shape (N, H, T, E/H)
+        V = V.reshape(N, T, self.n_head, self.head_dim).transpose(1, 2) # shape (N, H, T, E/H)
+
+        scores = Q @ K.transpose(2, 3) / math.sqrt(self.head_dim) # shape (N, H, S, T)
+        if attn_mask is not None:
+          scores = scores.masked_fill(attn_mask == 0, -float('inf'))
+        attention_weights = F.softmax(scores , dim=-1) # shape (N, H, S, T)
+        attention_weights = self.attn_drop(attention_weights) 
+        output = attention_weights @ V # shape (N, H, S, E/H)
+        output = output.transpose(1, 2) # shape (N, S, H, E/H)
+        output = self.proj(output.reshape(N, S, E)) # shape: (N, S, E)
+
+        return output
+
+
